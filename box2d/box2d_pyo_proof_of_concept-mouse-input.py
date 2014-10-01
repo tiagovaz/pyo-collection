feef038181c6e93213a4d9cde29cf848788c0e71
Tiago Bortoletto Vaz <tiago@debian.org>
2014-09-30 22:56:59 -0400

    still trying pyglet

--- box2d/box2d_pyo_proof_of_concept-mouse-input.py
+++ box2d/box2d_pyo_proof_of_concept-mouse-input.py
@@ -1,17 +1,23 @@
 import pyglet
 from pyglet import gl
+
 from Box2D import (
     b2Vec2, b2PolygonDef, b2World,
     b2BodyDef, b2AABB, b2MouseJointDef,
     b2CircleDef
 )
-import math
 import primitives
+import random
+import mapper
 
 # TODO:
-# - add many blocks
-# - draw ball
-# - draw line
+# - draw ball - OK!
+# - add objects from keyboard - OK!
+# - connect to libmapper - OK!
+# - draw lines
+
+#FIXME:
+# - lock from device creation
 
 # GLOBALS
 FPS = 60
@@ -35,7 +41,7 @@ class Main:
         world_bounds.upperBound = (200, 200)
         self.world = b2World(
             world_bounds,
-            b2Vec2(0, -30),  # Gravity vector
+            b2Vec2(0, -0),  # Gravity vector
             True  # Use "sleep" optimisation
         )
 
@@ -97,7 +103,6 @@ class Main:
                     break
 
         if body:
-            print "Block clicked"
             # A body was selected, create the mouse joint
             md = b2MouseJointDef()
             md.body1 = world.GetGroundBody()
@@ -117,6 +122,19 @@ class Main:
             p = surface.screen_to_world((x, y))
             self.mouse_joint.SetTarget(p)
 
+    def on_key_press(self, symbol, modifiers):
+        if symbol == pyglet.window.key._1:
+            self.objs.append(Block(random.uniform(1,5)))
+        if symbol == pyglet.window.key._2:
+            self.objs.append(Ball(random.uniform(1,5)))
+        #FIXME: doesn't update current objs
+        if symbol == pyglet.window.key.G:
+            self.set_gravity((0,-40))
+
+    def set_gravity(self, gravity):
+        # gravity is a tuple
+        self.world.SetGravity(b2Vec2(gravity))
+
     def on_draw(self):
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
         gl.glMatrixMode(gl.GL_MODELVIEW)
@@ -125,11 +143,10 @@ class Main:
         for obj in self.objs:
             obj.draw()
 
-
-
     def update(self, dt):
         self.world.Step(1.0 / FPS, 20, 16)
-        ball_body.ApplyForce(b2Vec2(0, 5), ball_body.position)
+        for obj in self.objs:
+            obj.poll()
 
 class Block():
     def __init__(self, box_size):
@@ -145,22 +162,41 @@ class Block():
         self.body = world.CreateBody(bodydef)
         shape = b2PolygonDef()
         shape.SetAsBox(box_size, box_size, (0, 0), 0)
-        shape.density = 0.1
-        shape.restitution = 0.2
+        shape.density = 0.4
+        shape.restitution = 0.5
         shape.friction = 0.5
 
         self.body.CreateShape(shape)
         self.body.SetMassFromShapes()
         surface.objs.append(self)
+        self.r, self.g, self.b = random.random(), random.random(), random.random()
+
+        self.map_block = mapper.device("Block")
+        #self.av = self.map_angularVelocity.add_output(abs(self.body.angularVelocity), 1, 'f', None, 0, 10)
+        self.av = self.map_block.add_output('/angularVelocity', 1, 'f', None, 0, 15)
+        self.angle = self.map_block.add_output('/angle', 1, 'f', None, 0, 300)
+        self.lvx = self.map_block.add_output('/linearVelocity.x', 1, 'f', None, 0, 50)
+        self.lvy = self.map_block.add_output('/linearVelocity.y', 1, 'f', None, 0, 50)
+        self.px = self.map_block.add_output('/position.x', 1, 'f', None, 2, 80)
+        self.py = self.map_block.add_output('/position.y', 1, 'f', None, 2, 60)
 
     def draw(self):
         transformed = [surface.world_to_screen(self.body.GetWorldPoint(p)) for p in self.points]
-        gl.glColor3f(0.4, 0.8, 0.4)
+        gl.glColor3f(self.r,self.g,self.b)
         gl.glBegin(gl.GL_QUADS)
         for p in transformed:
             gl.glVertex2f(*p)
         gl.glEnd()
 
+    def poll(self):
+#        print self.body
+        self.av.update(abs(self.body.angularVelocity))
+        self.angle.update(abs(self.body.angle))
+        self.lvx.update(abs(self.body.linearVelocity.x))
+        self.lvy.update(abs(self.body.linearVelocity.y))
+        self.px.update(self.body.position.x)
+        self.py.update(self.body.position.y)
+        self.map_block.poll(0)
 
 class Ball():
     def __init__(self, box_size):
@@ -176,40 +212,41 @@ class Ball():
         self.body = world.CreateBody(bodydef)
         self.cdef = b2CircleDef()
         self.cdef.radius = self.box_size
-        self.cdef.density = 0.1
-        self.cdef.restitution = 0.4
-        self.cdef.friction = 0.0
+        self.cdef.density = 0.4
+        self.cdef.restitution = 0.7
+        self.cdef.friction = 0.2
         self.body.CreateShape(self.cdef)
         self.body.SetMassFromShapes()
         surface.objs.append(self)
+        self.r, self.g, self.b = random.random(), random.random(), random.random()
 
     def draw(self):
-        print self.body
         center = ((-self.box_size + self.box_size) / 2, (self.box_size + -self.box_size) / 2)
         m = surface.world_to_screen(self.body.GetWorldPoint(center))
-        self.c = primitives.Circle(m[0],m[1],width=self.cdef.radius+40,color=(0.,.9,0.,1.),stroke=0)
-#        self.c.style = gl.GLU_LINE
+        self.c = primitives.Circle(m[0],m[1],width=self.cdef.radius*20,color=(self.r,self.g,self.b,1.),stroke=0)
+#        self.c.style = gl.GLU_POINT
         self.c.render()
+
+    def poll(self):
+        pass
+#        print self.body
 
 surface = Main()
 world = surface.world
 
-block = Block(5)
-block_body = block.body
-
-ball = Ball(2)
-ball_body = ball.body
-
-
 if __name__ == '__main__':
+    config = pyglet.gl.Config(sample_buffers=1, samples=4, depth_size=16, double_buffer=True)
     window = pyglet.window.Window(
         width=int(W / SCALE),
-        height=int(H / SCALE)
+        height=int(H / SCALE),
+        config=config,
+        resizable=True
     )
     window.event(surface.on_mouse_press)
     window.event(surface.on_mouse_release)
     window.event(surface.on_mouse_drag)
     window.event(surface.on_draw)
+    window.event(surface.on_key_press)
 
     pyglet.clock.schedule(surface.update)
     pyglet.app.run()
